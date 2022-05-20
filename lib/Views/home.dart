2ecf019837657620e20ef7bd108769db645c0002
M A <ma.paracha@hotmail.com>
2022-05-20 21:09:20 +0500

    Completed Future Builder (how we call api in future builder with Future Class)

--- lib/Views/home.dart
+++ lib/Views/home.dart
@@ -1,7 +1,17 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:future_builder/Modal/github_user.dart';
+import 'package:http/http.dart' as http;
+
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
+
+  Future<GithubUser> getData() async {
+      var response = await http.get(Uri.parse("https://api.github.com/users/m-ammar-p"));
+      return GithubUser.fromJson(jsonDecode(response.body));
+    } // getData
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +19,21 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Text("Future Builder"),
       ),
-      body: Text("Hello World"),
+      body: FutureBuilder<GithubUser>(
+        future: getData(),
+        builder: (childContext, snapshot){
+          // if Loading
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return Center(child: CircularProgressIndicator(),
+            );
+          }else if(snapshot.hasError){
+            return Text("Something is wrong :D");
+          }
+          else{
+            return Image.network(snapshot.data?.avatarUrl ?? "");
+          }
+        }, // builder
+      ),
     );
-  }
-}
+  } // build
+} // Home
